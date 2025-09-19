6ec8cb51ae003cd2460985f1205f72e241a6d2ee
adamnovacsek <nn53918@gmail.com>
2025-09-19 14:02:22 +0300

    fixsedsumstuff

--- pillarsAdjusted.js
+++ pillarsAdjusted.js
@@ -1,9 +1,20 @@
 var pillarBase = "██▓██▓██▓██";
-var pillarPeak ="░██▓██▓██▓██░";
+var pillarPeak = "░██▓██▓██▓██░";
+
+// Roughly estimate line height in pixels (depends on font/size)
+var lineHeight = 16; // adjust if needed
+// Total page height (viewport height here, can also use scrollHeight)
+var pageHeight = window.innerHeight;
+
+// How many base lines we need
+var pillarCount = Math.floor(pageHeight / lineHeight) - 2; // minus peaks
+
+document.write(pillarPeak + "\n");
+
+for (let i = 0; i < pillarCount; i++) {
+    document.write("   " + pillarBase + "\n");
+}
+
 document.write(pillarPeak + "\n");
 
 //V tejto verzii pillars scriptu sme schopni ich generovat normalne podseba s tym, ze ak su na krivo, da sa to cez css wrapper upravit tak, aby nam sedeli presne do columnu
-for (let i = 0; i < 60; i++) {
-    document.write("   "+pillarBase + "\n");
-}
-document.write(pillarPeak + "\n");
